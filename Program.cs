255450311183dc0ab639c10d5f1ee5eb9fd690f2
Jurandi Junior <jurandijr20@gmail.com>
2025-02-24 17:15:25 -0300

    Demo 1 - HashSet<T> e SortedSet<T>

--- Program.cs
+++ Program.cs
@@ -4,6 +4,19 @@
     {
         private static void Main(string[] args)
         {
+            Console.WriteLine();
+
+            Console.WriteLine("Demo 1 - HashSet<T> e SortedSet<T>");
+            HashSet<string> set = new HashSet<string>();
+            set.Add("TV");
+            set.Add("Notebook");
+            set.Add("Tablet");
+            Console.WriteLine(set.Contains("Notebook"));
+
+            foreach (String p in set)
+            {
+                Console.WriteLine(p);
+            }
         }
     }
 }
