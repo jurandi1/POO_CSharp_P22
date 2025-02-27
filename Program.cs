bef2255180447b5c50881fb12c74dabeda515c19
Jurandi Junior <jurandijr20@gmail.com>
2025-02-26 22:18:10 -0300

    Demo 2 - HashSet<T> e SortedSet<T>

--- Program.cs
+++ Program.cs
@@ -4,8 +4,30 @@
     {
         private static void Main(string[] args)
         {
-            Console.WriteLine();
+            Console.WriteLine("Demo 2 - HashSet<T> e SortedSet<T>");
 
+            SortedSet<int> a = new SortedSet<int>() { 0, 2, 4, 5, 6, 8, 10 };
+            SortedSet<int> b = new SortedSet<int>() { 5, 6, 7, 8, 9, 10 };
+
+            //union
+            Console.WriteLine("union");
+            SortedSet<int> c = new SortedSet<int>(a);
+            c.UnionWith(b);
+            PrintCollection(c);
+
+            //intersection
+            Console.WriteLine("intersection");
+            SortedSet<int> d = new SortedSet<int>(a);
+            d.IntersectWith(b);
+            PrintCollection(d);
+
+            //difference
+            Console.WriteLine("difference");
+            SortedSet<int> e = new SortedSet<int>(a);
+            e.ExceptWith(b);
+            PrintCollection(e);
+
+            Console.WriteLine();
             Console.WriteLine("Demo 1 - HashSet<T> e SortedSet<T>");
             HashSet<string> set = new HashSet<string>();
             set.Add("TV");
@@ -17,6 +39,15 @@
             {
                 Console.WriteLine(p);
             }
+        }
+
+        static void PrintCollection<T>(IEnumerable<T> collection)
+        {
+            foreach (T obj in collection)
+            {
+                Console.Write(obj + " ");
+            }
+            Console.WriteLine();
         }
     }
 }
